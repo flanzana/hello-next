76db91c9666b299299b1f156fd6331ad139770ba
Zana Flander <zana.flander@kiwi.com>
2020-03-01 17:04:22 +0100

    Add: style components

--- pages/index.js
+++ pages/index.js
@@ -28,36 +28,93 @@ import Layout from '../components/MyLayout';
 //   );
 // }
 
-const Index = props => (
-  <Layout>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.shows.map(show => (
-        <li key={show.id}>
-          <Link href="/p/[id]" as={`/p/${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </Layout>
+
+// const Index = props => (
+//   <Layout>
+//     <h1>Batman TV Shows</h1>
+//     <ul>
+//       {props.shows.map(show => (
+//         <li key={show.id}>
+//           <Link href="/p/[id]" as={`/p/${show.id}`}>
+//             <a>{show.name}</a>
+//           </Link>
+//         </li>
+//       ))}
+//     </ul>
+//   </Layout>
+// );
+//
+// /*
+//   In practice, we usually need to fetch data from a remote data source.
+//   Next.js comes with a standard API to fetch data for pages.
+//   We do it using an async function called getInitialProps.
+//  */
+// Index.getInitialProps = async function() {
+//   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+//   const data = await res.json();
+//
+//   // printed only on terminal console
+//   console.log(`Show data fetched. Count: ${data.length}`);
+//
+//   return {
+//     shows: data.map(entry => entry.show)
+//   };
+// };
+//
+// export default Index;
+
+
+function getPosts() {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+  ];
+}
+
+const PostLink = ({ post }) => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${post.id}`}>
+      <a>{post.title}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
 );
 
-/*
-  In practice, we usually need to fetch data from a remote data source.
-  Next.js comes with a standard API to fetch data for pages.
-  We do it using an async function called getInitialProps.
- */
-Index.getInitialProps = async function() {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  const data = await res.json();
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        {getPosts().map(post => (
+          <PostLink key={post.id} post={post} />
+        ))}
+      </ul>
+      <style jsx>{`
+        h1,
+        a {
+          font-family: 'Arial';
+        }
 
-  // printed only on terminal console
-  console.log(`Show data fetched. Count: ${data.length}`);
-
-  return {
-    shows: data.map(entry => entry.show)
-  };
-};
-
-export default Index;
+        ul {
+          padding: 0;
+        }
+      `}</style>
+    </Layout>
+  );
+}
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,27 +1,74 @@
 import { useRouter } from 'next/router';
 import fetch from 'isomorphic-unfetch';
+import Markdown from 'react-markdown';
 
 import Layout from '../../components/MyLayout';
 
 // Having brackets [] in the page name makes it a dynamic route.
 
-const Post = props => (
-  <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
-    {props.show.image ? <img src={props.show.image.medium} /> : null}
-  </Layout>
-);
+const Page = () => {
+  const router = useRouter();
+  console.log(router);
 
-Post.getInitialProps = async function(context) {
-  const { id } = context.query;
-  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
-  const show = await res.json();
+  return (
+    <Layout>
+      <h1>{router.query.id}</h1>
+      <div className="markdown">
+        <Markdown
+          source={`
+This is our blog post.
+Yes. We can have a [link](/link).
+And we can have a title as well.
 
-  // printed only in browser console (because of routing through Link)
-  console.log(`Fetched show: ${show.name}`);
+### This is a title
 
-  return { show };
+And here's the content.
+      `}
+        />
+      </div>
+      <style jsx global>{`
+        .markdown {
+          font-family: 'Arial';
+        }
+
+        .markdown a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        .markdown a:hover {
+          opacity: 0.6;
+        }
+
+        .markdown h3 {
+          margin: 0;
+          padding: 0;
+          text-transform: uppercase;
+        }
+      `}</style>
+    </Layout>
+  );
 };
 
-export default Post;
+export default Page;
+
+// const Post = props => (
+//   <Layout>
+//     <h1>{props.show.name}</h1>
+//     <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+//     {props.show.image ? <img src={props.show.image.medium} /> : null}
+//   </Layout>
+// );
+//
+// Post.getInitialProps = async function(context) {
+//   const { id } = context.query;
+//   const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+//   const show = await res.json();
+//
+//   // printed only in browser console (because of routing through Link)
+//   console.log(`Fetched show: ${show.name}`);
+//
+//   return { show };
+// };
+//
+// export default Post;
