4eb296346d209882f24f36bdbbd19998ab72bfe0
Zana Flander <zana.flander@kiwi.com>
2020-03-01 13:02:22 +0100

    Add: create Header component

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,10 @@
+import Header from "../components/Header";
+
 // this page will be open in localhost:3000/about
 export default function About() {
   return (
     <div>
+      <Header />
       <p>This is the about page</p>
     </div>
   );
