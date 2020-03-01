3f6b4b57c20f14a3cd15bda9df6b8ab03fdeaa2e
Zana Flander <zana.flander@kiwi.com>
2020-03-01 16:47:01 +0100

    Add: fetch data for pages

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,63 @@
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
 import Layout from '../components/MyLayout';
 
-// we are passing data via a query string parameter (a query param) in Link (example for post: /post?title=${props.title})
+// // we are passing data via a query string parameter (a query param) in Link (example for post: /post?title=${props.title})
+//
+// // clean URL: change from http://localhost:3000/post?title=Hello%20Next.js to http://localhost:3000/p/hello-nextjs (add "as" prop in Link)
+//
+// const PostLink = props => (
+//   <li>
+//     <Link href="/p/[id]" as={`/p/${props.id}`}>
+//       <a>{props.id}</a>
+//     </Link>
+//   </li>
+// );
+//
+// export default function Blog() {
+//   return (
+//     <Layout>
+//       <h1>My Blog</h1>
+//       <ul>
+//         <PostLink title="Hello Next.js" id="hello-nextjs" />
+//         <PostLink title="Learn Next.js is awesome" id="learn-nextjs" />
+//         <PostLink title="Deploy apps with Zeit" id="deploy-nextjs" />
+//       </ul>
+//     </Layout>
+//   );
+// }
 
-// clean URL: change from http://localhost:3000/post?title=Hello%20Next.js to http://localhost:3000/p/hello-nextjs (add "as" prop in Link)
-
-const PostLink = props => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${props.id}`}>
-      <a>{props.id}</a>
-    </Link>
-  </li>
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
 );
 
-export default function Blog() {
-  return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink title="Hello Next.js" id="hello-nextjs" />
-        <PostLink title="Learn Next.js is awesome" id="learn-nextjs" />
-        <PostLink title="Deploy apps with Zeit" id="deploy-nextjs" />
-      </ul>
-    </Layout>
-  );
-}
+/*
+  In practice, we usually need to fetch data from a remote data source.
+  Next.js comes with a standard API to fetch data for pages.
+  We do it using an async function called getInitialProps.
+ */
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  // printed only on terminal console
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,18 +1,27 @@
 import { useRouter } from 'next/router';
+import fetch from 'isomorphic-unfetch';
 
 import Layout from '../../components/MyLayout';
 
 // Having brackets [] in the page name makes it a dynamic route.
-const Page = () => {
-  const router = useRouter();
-  console.log(router);
 
-  return (
-    <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
-    </Layout>
-  );
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+    {props.show.image ? <img src={props.show.image.medium} /> : null}
+  </Layout>
+);
+
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  // printed only in browser console (because of routing through Link)
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
 };
 
-export default Page;
+export default Post;
