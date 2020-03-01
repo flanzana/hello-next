5b3e24654c07058db0974e062e618f2b8d516002
Zana Flander <zana.flander@kiwi.com>
2020-03-01 16:26:48 +0100

    Add: create clean URLs with Dynamic Routing

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,25 @@ import Link from 'next/link';
 import Layout from '../components/MyLayout';
 
 // we are passing data via a query string parameter (a query param) in Link (example for post: /post?title=${props.title})
+
+// clean URL: change from http://localhost:3000/post?title=Hello%20Next.js to http://localhost:3000/p/hello-nextjs (add "as" prop in Link)
+
 const PostLink = props => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
     </Link>
   </li>
 );
 
-export default function Index() {
+export default function Blog() {
   return (
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        <PostLink title="Hello Next.js" />
-        <PostLink title="Learn Next.js is awesome" />
-        <PostLink title="Deploy apps with Zeit" />
+        <PostLink title="Hello Next.js" id="hello-nextjs" />
+        <PostLink title="Learn Next.js is awesome" id="learn-nextjs" />
+        <PostLink title="Deploy apps with Zeit" id="deploy-nextjs" />
       </ul>
     </Layout>
   );
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,14 +1,15 @@
 import { useRouter } from 'next/router';
 
-import Layout from '../components/MyLayout';
+import Layout from '../../components/MyLayout';
 
+// Having brackets [] in the page name makes it a dynamic route.
 const Page = () => {
   const router = useRouter();
   console.log(router);
 
   return (
     <Layout>
-      <h1>{router.query.title}</h1>
+      <h1>{router.query.id}</h1>
       <p>This is the blog post content.</p>
     </Layout>
   );
