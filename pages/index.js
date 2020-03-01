cee763f6a57491208c646cc899819df66d2e535e
Zana Flander <zana.flander@kiwi.com>
2020-03-01 14:07:20 +0100

    Add: create dynamic pages for blog post using query strings

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
+import Link from 'next/link';
+
 import Layout from '../components/MyLayout';
+
+// we are passing data via a query string parameter (a query param) in Link (example for post: /post?title=${props.title})
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
 
 export default function Index() {
   return (
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
     </Layout>
   );
 }
